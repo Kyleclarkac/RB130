9b9c02038da769f3a4f5f3d5a3512349213d2657
Kyle Clark <kyleclarkac@gmail.com>
2021-04-01 12:50:30 -0400

    Tests

---
+++ Testing/car_test.rb
@@ -0,0 +1,34 @@
+require 'minitest/autorun'
+require 'minitest/reporters'
+Minitest::Reporters.use!
+
+require_relative 'car'
+
+class CarTest < MiniTest::Test
+  def test_wheels
+    car = Car.new
+    assert_equal(4, car.wheels)
+  end
+
+  def test_car_exists
+    car = Car.new
+    assert(car)
+  end
+
+  def test_name_is_nil
+    car = Car.new
+    assert_nil(car.name)
+  end
+
+  def test_raise_initialize
+    assert_raises(ArgumentError) do
+      car = Car.new(name: 'Joey')
+    end
+  end
+
+  def test_bad_wheels
+    skip
+    car = Car.new
+    assert_equal(3, car.wheels)
+  end
+end
